f96a07b56c1ed33b9c1a604b3de179d37390c577
3171997 <zferdous35@gmail.com>
2017-11-12 18:33:01 -0800

    Update circularlist.blade.php

--- resources/views/html/circularlist.blade.php
+++ resources/views/html/circularlist.blade.php
@@ -6,6 +6,6 @@
 			<center><a href='/circular/ViewCircular/Programming&Testing'><input type="button" value="Department of Programming & testing"/></a><br/></center>
 			<center><a href='/circular/ViewCircular/Graphics&Design'><input type="button" value="Department of Graphics & Desigining"/></a><br/><c/enter>
 			<center><a href='/circular/ViewCircular/Planning&analysis'><input type="button" value="Department of Planning & Analysis"/></a><br/></center>
-			<center><a href='/circular/ViewCircular/Interview Card'><input type="button" value="Interview Card"/></a><br/></center>
+			<center><a href='/circular/ViewCircular/InterviewCard'><input type="button" value="Interview Card"/></a><br/></center>
 		</section>
 @endsection
